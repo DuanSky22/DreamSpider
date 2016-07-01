43cab845c08051d51228bd517b95d5f22ca5b9bc
DuanSky <duansky22@163.com>
2016-07-01 15:56:54 +0800

    repush.

--- src/main/java/com/duansky/dreamspider/main/ParseEngine.java
+++ src/main/java/com/duansky/dreamspider/main/ParseEngine.java
@@ -7,19 +7,33 @@ package com.duansky.dreamspider.main;
 
 import com.duansky.dreamspider.store.StorerInFile;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ParseEngine {
 	
 	public static void main(String args[]){
+		int coreNumber = Runtime.getRuntime().availableProcessors();
+		//start the manager.
 		DreamSpiderInstance dsi=DreamSpider.newInstance();
 		Manager manager=new Manager(dsi);
 		manager.start();
-		
+		//start the watcher.
 		new Thread(new Watcher(manager)).start();
-		new Thread(new StorerInFile(manager)).start();
-		
-		while(true)
-			if(!manager.isAlive())
+		//start the storer.
+		ExecutorService es = Executors.newFixedThreadPool(coreNumber * 2);
+		for(int i = 0; i < coreNumber; i++)
+			es.submit(new StorerInFile(manager));
+
+		while(true) {
+			if (!manager.isAlive())
 				System.exit(0);
+			try {
+				Thread.sleep(5*1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
